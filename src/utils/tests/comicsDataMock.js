762ecf1d3ce0115328cf86a9d688990ff6c637de
Arthur D'Aquino <arthur.antunes@petco.com>
2021-02-05 10:00:51 -0300

    Creted mock data files for testing purposes

---
+++ src/utils/tests/comicsDataMock.js
@@ -0,0 +1,16 @@
+const comicData = [
+  {
+    title: 'Spider Man',
+    variantDescription: 'Anka Mighty Men Variant',
+    imgPath: 'http://i.annihil.us/u/prod/marvel/i/mg/b/a0/600f0ecde5b41.jpg',
+    imgAlt: 'Spider Man Issue'
+  },
+  {
+    title: 'Deadpool',
+    variantDescription: 'Deadpool variant description',
+    imgPath: 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg',
+    imgAlt: 'Spider Man Issue 2'
+  }
+]
+
+export default comicData
