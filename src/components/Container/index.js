a5a05c68c6e4484f3d3ce6f5388ece65d1610c90
Arthur D'Aquino <arthur.antunes@petco.com>
2021-02-06 10:27:41 -0300

    Created component Container

---
+++ src/components/Container/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Container = styled.div.attrs({
+  className: 'Container',
+})`
+  width: 100%;
+  max-width: 87.5rem;
+  padding: 0 1rem;
+  margin-right: auto;
+  margin-left: auto;
+`
+
+export default Container
