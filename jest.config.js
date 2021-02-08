40fedca65c294296f1d05835df0b407cbdf181e7
Arthur D'Aquino <arthur.antunes@petco.com>
2021-02-08 13:45:18 -0300

    Updated jest.config.js to ignore styles files

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,13 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.js(x)?', '!src/**/stories.jsx', '!src/styles/*.js'],
+  collectCoverageFrom: [
+    'src/**/*.js(x)?',
+    '!src/**/stories.jsx',
+    '!src/**/styles/*.js',
+    '!src/**/styles.js',
+    '!src/utils/**/*.js'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.js'],
   modulePaths: ['<rootDir>/src/']
 }
